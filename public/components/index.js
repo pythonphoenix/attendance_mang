201a7211ddbcced881561e8ef6f081bc869e4b6f
Python_phoenix <olanipekunvictory123@gmail.com>
2021-05-07 04:44:13 +0100

    push

---
+++ public/components/index.js
@@ -0,0 +1,182 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+function landingPage() {
+	return (
+		<> 
+		
+		  <main class="side-main">
+		       
+		    <section class="hero-banner mb-30px">
+		      <div class="container">
+		        <div class="row">
+		          <div class="col-lg-7">
+		            <div class="hero-banner__img">
+		              <img class="img-fluid" src="../img/banner/hero-banner.png" alt="landingImage"/>
+		            </div>
+		          </div>
+
+		          <div class="col-lg-5 pt-5">
+		            <div class="hero-banner__content">
+		              <h1>Attendance Management System</h1>
+		              <p>Whether its punching in to work or requesting a correction for an accidental punch-out, checking on leave balances or checking the attendance report for the month, FieldSense has it all covered for you. </p>
+		              <a class="button bg" href="#">Get Started</a>
+		            </div>
+		          </div>
+		        </div>
+		      </div>
+		    </section>
+		         
+		    <section class="section-margin">
+		      <div class="container">
+		        <div class="section-intro pb-85px text-center">
+		          <h2 class="section-intro__title">Flexible Timesheet Capture</h2>
+		          <p class="section-intro__subtitle">It’s fast and easy to record hours spent on any activity, project or task using Replicon’s optimized user interfaces for your phone, tablet or computer. Fully customizable, Replicon time tracking software meets your exact needs, working for all types of employees and any timesheet format.</p>
+		        </div>
+
+		        <div class="container">
+		          <div class="row">
+		            <div class="col-lg-4">
+		              <div class="card card-feature text-center text-lg-left mb-4 mb-lg-0">
+		                <span class="card-feature__icon">
+		                  <i class="ti-package"></i>
+		                </span>
+		                <h3 class="card-feature__title">Unique Design</h3>
+		                <p class="card-feature__subtitle">Molestie lorem est faucibus faucibus erat phasellus placerat proin aptent</p>
+		              </div>
+		            </div>
+		            <div class="col-lg-4">
+		              <div class="card card-feature text-center text-lg-left mb-4 mb-lg-0">
+		                <span class="card-feature__icon">
+		                  <i class="ti-mouse-alt"></i>
+		                </span>
+		                <h3 class="card-feature__title">Business Solution</h3>
+		                <p class="card-feature__subtitle">Molestie lorem est faucibus faucibus erat phasellus placerat proin aptent</p>
+		              </div>
+		            </div>
+		            <div class="col-lg-4">
+		              <div class="card card-feature text-center text-lg-left mb-4 mb-lg-0">
+		                <span class="card-feature__icon">
+		                  <i class="ti-headphone-alt"></i>
+		                </span>
+		                <h3 class="card-feature__title">Customer Support</h3>
+		                <p class="card-feature__subtitle">Molestie lorem est faucibus faucibus erat phasellus placerat proin aptent</p>
+		              </div>
+		            </div>
+		          </div>
+		        </div>
+		      </div>
+		    </section>
+		        
+		    <section class="section-padding--small bg-magnolia">
+		      <div class="container">
+		        <div class="row no-gutters align-items-center">
+		          <div class="col-md-5 mb-5 mb-md-0">
+		            <div class="about__content">
+		              <h2>100% Accurate Time</h2>
+		              <p>To ensure data is error-free and is ready for processing, Replicon time tracking software helps to verify, cleanse, analyze, review, and apply the right workflows to your time information, and provides a built-in compliance and rules engine to adhere to key business policies.</p>
+		              <a class="button button-light" href="#">Know More</a>
+		            </div>
+		          </div>
+		          <div class="col-md-7">
+		            <div class="about__img">
+		              <img class="img-fluid" src="../img/home/h.png" alt="">
+		            </div>
+		          </div>
+		        </div>
+		      </div>
+		    </section>
+		    <!--================ about section end =================-->      
+		    
+		    <!--================ Offer section start =================-->      
+		    <section class="section-margin">
+		      <div class="container">
+		        <div class="section-intro pb-85px text-center">
+		          <h2 class="section-intro__title">Features We Offer</h2>
+		          <p class="section-intro__subtitle">Vel aliquam quis, nulla pede mi commodo tristique nam hac. Luctus torquent velit felis commodo pellentesque nulla cras. Tincidunt hacvel alivquam quis nulla pede mi commodo tristique nam hac  luctus torquent</p>
+		        </div>
+
+		        <div class="row">
+		          <div class="col-lg-6">
+
+		            <div class="row offer-single-wrapper">
+		              <div class="col-lg-6 offer-single">
+		                <div class="card offer-single__content text-center">
+		                  <span class="offer-single__icon">
+		                    <i class="ti-pencil-alt"></i>
+		                  </span>
+		                  <h4>Easy To Manage</h4>
+		                  <p>Posuere porttitor justo ornare dictum ultricies enim imperdiet integer habitant.</p>
+		                </div>
+		              </div>
+		              
+		              <div class="col-lg-6 offer-single">
+		                <div class="card offer-single__content text-center">
+		                  <span class="offer-single__icon">
+		                    <i class="ti-ruler-pencil"></i>
+		                  </span>
+		                  <h4>Analytics Tool</h4>
+		                  <p>Posuere porttitor justo ornare dictum ultricies enim imperdiet integer habitant.</p>
+		                </div>
+		              </div>
+		            </div>
+
+		            <div class="row offer-single-wrapper">
+		              <div class="col-lg-6 offer-single">
+		                <div class="card offer-single__content text-center">
+		                  <span class="offer-single__icon">
+		                    <i class="ti-cut"></i>
+		                  </span>
+		                  <h4>Professionals Tools</h4>
+		                  <p>Posuere porttitor justo ornare dictum ultricies enim imperdiet integer habitant.</p>
+		                </div>
+		              </div>
+		              
+		              <div class="col-lg-6 offer-single">
+		                <div class="card offer-single__content text-center">
+		                  <span class="offer-single__icon">
+		                    <i class="ti-light-bulb"></i>
+		                  </span>
+		                  <h4>Ready Content</h4>
+		                  <p>Posuere porttitor justo ornare dictum ultricies enim imperdiet integer habitant.</p>
+		                </div>
+		              </div>
+		            </div>
+
+		          </div>
+		          <div class="col-lg-6">
+		            <div class="offer-single__img">
+		              <img class="img-fluid" src="../img/home/offer.png" alt="">
+		            </div>
+		          </div>
+		        </div>
+		      </div>
+		    </section>
+		         
+		    <section class="section-padding--small bg-magnolia">
+		      <div class="container">
+		        <div class="row align-items-center pt-xl-3 pb-xl-5">
+		          <div class="col-lg-6">
+		            <div class="solution__img text-center text-lg-left mb-4 mb-lg-0">
+		              <img class="img-fluid" src="../img/home/solution.png" alt="">
+		            </div>
+		          </div>
+		          <div class="col-lg-6">
+		            <div class="solution__content">
+		              <h2>Simple Solutions for Complex Connections</h2>
+		              <p>Molestie lorem est faucibus Faucibus erat phasellus placerat proinint aptent pulvinar fusce nostra porta sem platea nec, donec fusce erat Molestie lorem est faucibus faucibus erat phasellus placerat proin aptent pulvinar fusce nostra port</p>
+		              <a class="button button-light" href="#">Know More</a>
+		            </div>
+		          </div>
+		        </div>
+		      </div>
+		    </section>
+		</>    
+
+		
+
+		)
+}
